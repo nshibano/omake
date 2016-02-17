4623035de612443429ec9f1f93131d1dcda78ce3
Nozomi SHIBANO <shibano.nozomi@gmail.com>
2016-02-18 00:29:29 +0900

    Initialization of top
    Now top is initialized with actually evaluating
    ocaml code, not assignment to Clflags.include_dirs.
    Evaluating ocaml code is more general and extendable.

--- src/top/boot_top.ml
+++ src/top/boot_top.ml
@@ -1,6 +1,29 @@
+let builddir = Top_config.builddir
 
+let include_dirs = let (//) = Filename.concat in [
+	builddir // "src"// "ast" ; 
+	builddir // "src"// "build" ; 	
+	builddir // "src"// "util" ; 	
+	builddir // "src"// "builtin" ; 	
+	builddir // "src"// "clib" ; 	
+	builddir // "src"// "env" ; 		
+	builddir // "src"// "eval" ; 		
+	builddir // "src"// "exec" ; 		
+	builddir // "src"// "ir" ; 		
+	builddir // "src"// "libmojave" ; 		
+	builddir // "src"// "magic" ; 		
+	builddir // "src"// "main" ; 		
+	builddir // "src"// "shell" ; 		
+]
 
-let () = 
+let eval_exn str =
+  let lexbuf = Lexing.from_string str in
+  let phrase = !Toploop.parse_toplevel_phrase lexbuf in
+  Toploop.execute_phrase false Format.err_formatter phrase
+
+let _ = 
   begin
-    Clflags.include_dirs := Top_config.include_dirs @ !Clflags.include_dirs ;
+    List.iter (fun dir ->
+      ignore (eval_exn (Printf.sprintf {s|#directory {|%s|};;|s} dir))) include_dirs;
+    ignore (eval_exn {s|#install_printer Lm_symbol.dump_symbol;;|s})
   end
